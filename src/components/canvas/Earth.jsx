613b20ff04c2ce64e6a59df4b77095ebf4d645f5
Muhammad Talha Masood <94213819+Crazy-Coder8665@users.noreply.github.com>
2023-06-09 16:57:23 +0500

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 import CanvasLoader from "../Loader"
 
 const Earth = ({isMobile}) => {
-  const earth = useGLTF("./earth/scene.gltf")
+  const earth = useGLTF("./earth/scene.glb")
   return(
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
